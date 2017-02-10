0ce50e3afdd24e1a24311a23a517fea7af56cfb8
阿久津岳志 <sky_10577@yahoo.co.jp>
2017-02-10 17:32:23 +0900

    create dashboard

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
-  get 'templates/index'
+  root 'templates#index'
+  get '/templates/dashboard' => 'templates#dashboard'
+  get '/templates/todo_list' => 'templates#todo_list'
 
   namespace :api, defaults: { format: :json } do
-    resources :todo_lists, only: :show do
+    resources :todo_lists, only: [:index, :show, :create, :destroy] do
       resources :todos, except: [:index, :new, :edit, :show]
     end
   end
